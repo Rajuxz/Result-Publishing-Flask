038ee0e70cdf61c9cbef6dea28326ab664a0e89d
Rajuxz <Rajankhanalxz@gmail.com>
2023-04-09 20:26:54 +0545

    Flask APP

---
+++ static/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('form').on('submit',function(event){
+      $.ajax({
+        data:{
+          email:$('#email_add').val(),
+          password: $('#password').val()
+        },
+        type: 'POST',
+        url: '/sign_in'
+      })
+      .done(function(data){
+        if(data.error){
+          $('#failure-alert').text(data.error).show();
+          $('#success-alert').hide()
+
+            //$('p').show()
+        }else{
+          $('#success-alert').text(data.email).show();
+          $('#failure-alert').hide()
+        }
+      });
+      event.preventDefault();
+    });
+  });
